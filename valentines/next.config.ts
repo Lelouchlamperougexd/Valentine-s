7ea1122016acf43123aefeeb13f103600ef456df
Lelouchlamperougexd <119339442+Lelouchlamperougexd@users.noreply.github.com>
2026-02-14 20:16:33 +0500

    Add GitHub Pages deployment

--- valentines/next.config.ts
+++ valentines/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Your Next.js config
+  output: "export",
+  basePath: "/Valentine-s",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
